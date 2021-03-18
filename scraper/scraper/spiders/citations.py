536e114a97a1490f21535baed0e61c399d172d05
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2021-03-18 20:03:10 +0100

    Cleaning citation content from HTML via regex

--- scraper/scraper/spiders/citations.py
+++ scraper/scraper/spiders/citations.py
@@ -1,10 +1,13 @@
+import re
 import scrapy
+
 
 class CitationSpider(scrapy.Spider):
     name = 'citations'
     start_urls = [
         'https://demagog.org.pl/wypowiedzi/'
     ]
+    pattern = re.compile(r'(</?[a-z].+?>)+')
 
     def parse(self, response):
         detail_urls = response.css('.title-archive a')
@@ -14,20 +17,20 @@ class CitationSpider(scrapy.Spider):
         yield from response.follow_all(next_page_url, callback=self.parse)
 
     def parse_detail(self, response):
-        content = response.css('.hyphenate p::text').get()
-        if not content or len(content) < 5:
-            content = response.css('.hyphenate span::text').get()
-        
-        if not content or len(content) < 5:
-            # last is cookie consent
-            content = ''.join(response.css('.hyphenate span::text').getall()[:-1])
+        content = response.css('.hyphenate').get()
+        date = response.css('.date-content').get()
 
-        # remove non breaking space 
-        content = content.replace('\xa0', ' ').strip()
+        # remove date tags and text
+        content = content.replace(date, '')
+
+        # clean all html tags
+        content = self.pattern.sub('', content)
+
+        # get rid of nbsp and \n
+        content = content.replace('\xa0', ' ').replace('\n', ' ').strip()
 
         yield {
             'content': content,
             'author': response.css('.person-name a::text').get(),
             'label': response.css('.ocena::text').get()
         }
-
