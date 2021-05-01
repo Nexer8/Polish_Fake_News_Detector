d9d71aa80dc258cb6192c6857683f191afd34402
szymwisn <szymwisn@gmail.com>
2021-05-01 10:39:17 +0200

    FNC-1 Storybook styles

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,10 +1,35 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyle from '../src/theme/GlobalStyle';
+import { theme } from '../src/theme/mainTheme';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+  backgrounds: {
+    default: 'dark',
+    values: [
+      {
+        name: 'dark',
+        value: 'hsl(220, 9%, 13%)',
+      },
+    ],
+  },
+};
+
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Story />
+      </ThemeProvider>
+    </>
+  ),
+];
