4c49cb973c895aba64022b0060c832e3686dfd09
szymwisn <szymwisn@gmail.com>
2021-05-01 13:29:12 +0200

    FNC-1 Font sizes in mainTheme fix

--- client/src/theme/mainTheme.ts
+++ client/src/theme/mainTheme.ts
@@ -15,11 +15,9 @@ export const theme = {
     regular: 400,
   },
   fontSize: {
-    xxs: '1rem',
-    xs: '1.2rem',
-    s: '1.6rem',
-    m: '2.1rem',
-    l: '2.4rem',
-    xl: '4rem',
+    s: '1.4rem',
+    m: '1.6rem',
+    xl: '2.4rem',
+    xxl: '3.6rem',
   },
 };
