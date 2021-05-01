bd5c8bd7c8a57156eadd06246ada5bd408f6a6b6
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2021-05-01 19:08:37 +0200

    FNC-11 Done statement evaluation component

--- client/src/components/StatementEvaluation.tsx
+++ client/src/components/StatementEvaluation.tsx
@@ -1,9 +1,66 @@
 import React from 'react';
+import styled from 'styled-components';
 
-interface Props {
-  // TODO: define props here
+export interface Props {
+  // we still dunno how many classes are there, so cannot if-else that
+  backgroundColor: string;
+  probability: number;
+  verdict: string;
 }
 
-export const StatementEvaluation: React.FC<Props> = () => {
-  return <div></div>;
+const Container = styled.div<{ backgroundColor: string }>`
+  display: flex;
+  justify-content: center;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  font-family: 'Playfair Display';
+  width: 500px;
+  height: 170px;
+  border-radius: 4px;
+`;
+
+const BaseColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0 25px;
+`;
+
+const Verdict = styled(BaseColumn)`
+  span {
+    font-size: ${({ theme }) => theme.fontSize.xxl};
+    color: ${({ theme }) => theme.colors.gray};
+  }
+`;
+
+const Explanation = styled(BaseColumn)`
+  span {
+    display: block;
+    font-size: ${({ theme }) => theme.fontSize.xl};
+    text-align: center;
+
+    &:last-child {
+      margin-top: 20px;
+    }
+  }
+`;
+
+const Row = styled.span``;
+
+export const StatementEvaluation: React.FC<Props> = ({
+  backgroundColor,
+  probability,
+  verdict,
+}) => {
+  return (
+    <Container backgroundColor={backgroundColor}>
+      <Verdict>
+        <span>{verdict}</span>
+      </Verdict>
+      <Explanation>
+        <span>Z&nbsp;prawdopodobnieństwem</span>
+        <span>{probability}%</span>
+      </Explanation>
+    </Container>
+  );
 };
