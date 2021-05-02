763c28dec9c633d63d668bf04baf26528b68b214
szymwisn <szymwisn@gmail.com>
2021-05-02 17:30:54 +0200

    FNC-39 Navigation component

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,9 +1,69 @@
 import React from 'react';
+import styled from 'styled-components';
 
-interface Props {
-  // TODO: define props here
+import menuIcon from 'icons/menu-darker.svg';
+import { Icon } from './Icon';
+import { Button } from './Button';
+import { theme } from 'theme/mainTheme';
+
+export type NavigationItem = {
+  name: string;
+  icon: string;
+};
+
+export interface Props {
+  items: NavigationItem[];
+  selectedItem: NavigationItem;
+  onItemClick: (item: NavigationItem) => void;
 }
 
-export const Navigation: React.FC<Props> = () => {
-  return <div></div>;
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledTitle = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 24px;
+  color: ${({ theme }) => theme.colors.lightDark};
+  font-size: ${({ theme }) => theme.fontSize.s};
+
+  span {
+    margin-left: 11px;
+  }
+`;
+
+const StyledButton = styled.div`
+  margin-bottom: 10px;
+`;
+
+export const Navigation: React.FC<Props> = ({
+  selectedItem,
+  items,
+  onItemClick,
+}) => {
+  return (
+    <StyledWrapper>
+      <StyledTitle>
+        <Icon svg={menuIcon} alt="Nawigacja" />
+        <span>Nawigacja</span>
+      </StyledTitle>
+      {items.map((item) => (
+        <StyledButton key={item.name}>
+          <Button
+            backgroundColor={
+              item.name === selectedItem.name
+                ? theme.colors.lightDark
+                : theme.colors.mediumDark
+            }
+            title={item.name}
+            icon={item.icon}
+            isFullWidth
+            onClick={() => onItemClick(item)}
+          />
+        </StyledButton>
+      ))}
+    </StyledWrapper>
+  );
 };
