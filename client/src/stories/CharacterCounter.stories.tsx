a435859814ebfdc559faa48b84ac8cc406f23557
szymwisn <szymwisn@gmail.com>
2021-05-02 18:05:27 +0200

    FNC-24 CharacterCounter component

---
+++ client/src/stories/CharacterCounter.stories.tsx
@@ -0,0 +1,25 @@
+import { Story, Meta } from '@storybook/react';
+
+import {
+  CharacterCounter,
+  Props,
+} from 'client-pages/StatementVerifier/CharacterCounter';
+
+export default {
+  title: 'StatementVerifier/CharacterCounter',
+  component: CharacterCounter,
+} as Meta;
+
+const Template: Story<Props> = (args) => <CharacterCounter {...args} />;
+
+export const Valid = Template.bind({});
+Valid.args = {
+  currentCount: 5,
+  maxCharacters: 255,
+};
+
+export const Invalid = Template.bind({});
+Invalid.args = {
+  currentCount: 267,
+  maxCharacters: 255,
+};
