a6c45d9d14c23e0b59dca9c6dabe59f6232c5a0c
Filip <filipgajewski4@gmail.com>
2021-05-13 09:22:24 +0200

    Remove unused code

--- models/mlp.py
+++ models/mlp.py
@@ -55,31 +55,3 @@ def predict_single_instance(model, instance):
 
     return [round(prc, 4) for prc in prediction[0]]
 
-
-# if __name__ == '__main__':
-#     df = pd.read_csv('../data/clean.csv')
-#     X_train, X_test, y_train, y_test = train_test_split(df['clean_text'], df['label'], test_size=0.25)
-
-#     tokenizer = Tokenizer()
-#     tokenizer.fit_on_texts(X_train)
-#     X_train_seq = tokenizer.texts_to_sequences(X_train)
-#     X_test_seq = tokenizer.texts_to_sequences(X_test)
-
-#     padding_length = len(max(X_train_seq, key=len))
-
-#     X_train_seq_pad = pad_sequences(X_train_seq, padding_length)
-#     X_test_seq_pad = pad_sequences(X_test_seq, padding_length)
-
-#     y_train_cat = to_categorical(y_train, 2)
-#     y_test_cat = to_categorical(y_test, 2)
-
-#     model = create_mlp_model()
-
-#     for numb in range(100, 140):
-#         print('Treść wypowiedzi:')
-#         print(X_test.iloc[numb])
-#         print('Klasyfikacja: ', y_test.iloc[numb])
-#         prediction = model.predict(np.array([X_test_seq_pad[numb]]))
-#         print('Predykcja: ', [round(prc, 5) for prc in prediction[0]])
-#         # print(X_test_seq_pad[numb])
-#         print('================================================')
