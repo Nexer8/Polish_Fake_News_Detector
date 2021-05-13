f027ecdd16c4e6916f6405b650149109f47f4f2b
szymwisn <szymwisn@gmail.com>
2021-05-13 17:49:40 +0200

    FNC-31 Report Page - preview

--- client/src/client-pages/ResultReport/index.tsx
+++ client/src/client-pages/ResultReport/index.tsx
@@ -17,9 +17,15 @@ import { Textarea } from 'components/Textarea';
 import { DateInput } from 'components/DateInput';
 import { DropdownItem, Select } from 'components/Select';
 import { Button } from 'components/Button';
+import {
+  StatementEvaluation,
+  VerdictType,
+} from 'components/StatementEvaluation';
 
 const EMAIL_FIELD: string = 'email';
 const COMMENT_FIELD: string = 'comment';
+const NAVIGATION_ITEM_FORM: string = 'Formularz';
+const NAVIGATION_ITEM_PREVIEW: string = 'Zgłaszany wynik';
 const POLITICIAN_FIELD: string = 'politician';
 const DATE_FIELD: string = 'date';
 const CATEGORY_FIELD: string = 'category';
@@ -32,15 +38,16 @@ interface Props {
 
 const navigationItems = [
   {
-    name: 'Formularz',
+    name: NAVIGATION_ITEM_FORM,
     icon: penIcon,
   },
   {
-    name: 'Zgłaszany wynik',
+    name: NAVIGATION_ITEM_PREVIEW,
     icon: eyeIcon,
   },
 ];
 
+// TODO: provide valid categories (prossibly fetch from backend)
 const categories: DropdownItem[] = [
   {
     name: 'Lorem',
@@ -59,7 +66,7 @@ const categories: DropdownItem[] = [
   },
 ];
 
-const StyledForm = styled.form`
+const StyledWrapper = styled.div`
   display: flex;
   flex-direction: column;
   max-width: 500px;
@@ -92,6 +99,10 @@ const StyledButtons = styled.div`
   display: flex;
   width: 100%;
   justify-content: end;
+  margin: 30px 0;
+`;
+
+const StyledEvaluationWrapper = styled.div`
   margin: 30px 0;
 `;
 
@@ -131,61 +142,81 @@ export const ResultReport: React.FC<Props> = () => {
         />
       }
     >
-      <StyledForm onSubmit={formik.handleSubmit}>
+      <StyledWrapper>
         <ReturnButton text="Wróć do wyniku" path="TODO: provide path" />
-        <StyledHeading>Formularz zgłoszenia</StyledHeading>
-        <StyledTextDisplay isBgDark>{TEXT_DISPLAY_VALUE}</StyledTextDisplay>
-        <Input
-          icon={mailIcon}
-          id={EMAIL_FIELD}
-          name={EMAIL_FIELD}
-          onChange={formik.handleChange}
-          placeholder="Wprowadź swój adres e-mail"
-          type={EMAIL_FIELD}
-          value={formik.values[EMAIL_FIELD].toString()}
-        />
-        <StyledTextarea
-          id={COMMENT_FIELD}
-          name={COMMENT_FIELD}
-          onChange={formik.handleChange}
-          placeholder="Wprowadź komentarz dotyczący zgłoszenia."
-          value={formik.values[COMMENT_FIELD].toString()}
-        />
-        <StyledHeading>Dodatkowe informacje</StyledHeading>
-        <Input
-          icon={userIcon}
-          id={POLITICIAN_FIELD}
-          name={POLITICIAN_FIELD}
-          onChange={formik.handleChange}
-          placeholder="Podaj imię i nazwisko polityka"
-          type={POLITICIAN_FIELD}
-          value={formik.values[POLITICIAN_FIELD].toString()}
-        />
-        <StyledDateInput
-          className={DATE_FIELD}
-          id={DATE_FIELD}
-          name={DATE_FIELD}
-          onChange={formik.handleChange}
-          placeholder="Podaj imię i nazwisko polityka"
-          value={formik.values[DATE_FIELD].toString()}
-        />
-        <Select
-          items={categories}
-          selectedItem={formik.values[CATEGORY_FIELD] as DropdownItem}
-          placeholder="Wybierz kategorię wypowiedzi"
-          onSelect={(item: DropdownItem) =>
-            formik.setFieldValue(CATEGORY_FIELD, item)
-          }
-        />
-        <StyledButtons>
-          <Button
-            title="Anuluj zgłoszenie"
-            icon={cancelIcon}
-            onClick={handleCancel}
-          />
-          <Button type="submit" title="Wyślij zgłoszenie" icon={sendIcon} />
-        </StyledButtons>
-      </StyledForm>
+
+        {navigationSelectedItem.name === NAVIGATION_ITEM_FORM && (
+          <form onSubmit={formik.handleSubmit}>
+            <StyledHeading>Formularz zgłoszenia</StyledHeading>
+            <StyledTextDisplay isBgDark>{TEXT_DISPLAY_VALUE}</StyledTextDisplay>
+            <Input
+              icon={mailIcon}
+              id={EMAIL_FIELD}
+              name={EMAIL_FIELD}
+              onChange={formik.handleChange}
+              placeholder="Wprowadź swój adres e-mail"
+              type={EMAIL_FIELD}
+              value={formik.values[EMAIL_FIELD].toString()}
+            />
+            <StyledTextarea
+              id={COMMENT_FIELD}
+              name={COMMENT_FIELD}
+              onChange={formik.handleChange}
+              placeholder="Wprowadź komentarz dotyczący zgłoszenia."
+              value={formik.values[COMMENT_FIELD].toString()}
+            />
+            <StyledHeading>Dodatkowe informacje</StyledHeading>
+            <Input
+              icon={userIcon}
+              id={POLITICIAN_FIELD}
+              name={POLITICIAN_FIELD}
+              onChange={formik.handleChange}
+              placeholder="Podaj imię i nazwisko polityka"
+              type={POLITICIAN_FIELD}
+              value={formik.values[POLITICIAN_FIELD].toString()}
+            />
+            <StyledDateInput
+              className={DATE_FIELD}
+              id={DATE_FIELD}
+              name={DATE_FIELD}
+              onChange={formik.handleChange}
+              placeholder="Podaj imię i nazwisko polityka"
+              value={formik.values[DATE_FIELD].toString()}
+            />
+            <Select
+              items={categories}
+              selectedItem={formik.values[CATEGORY_FIELD] as DropdownItem}
+              placeholder="Wybierz kategorię wypowiedzi"
+              onSelect={(item: DropdownItem) =>
+                formik.setFieldValue(CATEGORY_FIELD, item)
+              }
+            />
+            <StyledButtons>
+              <Button
+                title="Anuluj zgłoszenie"
+                icon={cancelIcon}
+                onClick={handleCancel}
+              />
+              <Button type="submit" title="Wyślij zgłoszenie" icon={sendIcon} />
+            </StyledButtons>
+          </form>
+        )}
+
+        {navigationSelectedItem.name === NAVIGATION_ITEM_PREVIEW && (
+          <>
+            <StyledHeading>Podejrzana wypowiedź</StyledHeading>
+            <StyledTextDisplay>TODO: Statement text</StyledTextDisplay>
+
+            <h2>Ocena wypowiedzi przez model</h2>
+            <StyledEvaluationWrapper>
+              <StatementEvaluation
+                probability={70}
+                verdict={VerdictType.FAKE}
+              />
+            </StyledEvaluationWrapper>
+          </>
+        )}
+      </StyledWrapper>
     </SidebarTemplate>
   );
 };
