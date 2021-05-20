d6ced1712fce81c4ba6aee61ca0c954566e54679
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2021-05-20 22:42:37 +0200

    Added result model

--- server/models/Result.js
+++ server/models/Result.js
@@ -1,7 +1,22 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const resultSchema = new Schema({});
+const CANNOT_BE_EMPTY_MSG = "can't be blank.";
+
+const resultSchema = new Schema({
+  statement: {
+    type: String,
+    required: [true, CANNOT_BE_EMPTY_MSG],
+  },
+  verdict: {
+    type: String,
+    required: [true, CANNOT_BE_EMPTY_MSG],
+  },
+  probability: {
+    type: Number,
+    required: [true, CANNOT_BE_EMPTY_MSG],
+  },
+});
 
 const Result = mongoose.model("Result", resultSchema);
 
