9682310c212c75ff685e1daa8308e06d05f8193c
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2021-05-20 22:35:18 +0200

    FNB-5 Added Report model

--- server/models/Report.js
+++ server/models/Report.js
@@ -1,8 +1,33 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const reportSchema = new Schema({});
+const reportSchema = new Schema({
+  statement: {
+    type: String,
+    required: [true, CANNOT_BE_EMPTY_MSG],
+  },
+  verdict: {
+    type: String,
+    required: [true, CANNOT_BE_EMPTY_MSG],
+  },
+  probability: {
+    type: Number,
+    required: [true, CANNOT_BE_EMPTY_MSG],
+  },
+  reporter: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: [true, CANNOT_BE_EMPTY_MSG],
+    match: [/\S+@\S+\.\S+/, "is invalid."],
+    index: true,
+  },
+  comment: String,
+  politician: String,
+  date: Date,
+  category: String,
+});
 
-const Report = mongoose.model("Report", reportsSchema);
+const Report = mongoose.model("Report", reportSchema);
 
 module.exports = Report;
